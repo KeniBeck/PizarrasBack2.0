0716e26beec1e683025dd52e03cc03b7669eae0d
Deyler Mena <deyler_mena69222@elpoli.edu.co>
2025-04-15 22:04:35 -0500

    add auth and login modules with controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { UserModule } from './user/user.module';
 import { TikectBuyModule } from './tikect-buy/tikect-buy.module';
+import { AuthModule } from './auth/auth.module';
+import { LoginModule } from './login/login.module';
 
 @Module({
-  imports: [UserModule, TikectBuyModule],
+  imports: [UserModule, TikectBuyModule, AuthModule, LoginModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,18 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+import { IsInt, IsNumber, IsString } from "class-validator";
+
+export class CreateUserDto {
+    @ApiProperty({example:"1"})
+    @IsNumber()
+    @Type(() => Number)
+    id: number;
+
+    @ApiProperty({example:"John"})
+    @IsString()
+    username: string;
+
+    @ApiProperty({example:"********"})
+    @IsString()
+    password: string;
+}
